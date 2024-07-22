cec9ae4ff23f3d5d0c285f37965e6dec1ebd9d54
glbreeze <gl.breeze@gmail.com>
2024-07-22 00:46:06 -0400

    update

--- train.py
+++ train.py
@@ -54,7 +54,7 @@ def main(args):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     train_loader, val_loader = get_dataloader(args)
-    if args.dataset == 'swimmer' or args.dataset == 'reacher':
+    if args.dataset in ['swimmer', 'reacher', 'reacher_ab', 'swimmer_ab']:
         args.num_x = train_loader.dataset.state_dim
         if args.which_y == -1: 
             args.num_y = train_loader.dataset.action_dim
